4cbad26d88817a050286ab7d2057310c40ad80bf
Frietz <151024583+CodeByFrietz@users.noreply.github.com>
2026-09-24 06:16:21 +0800

    Update Supabase key to a publishable key

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@
     "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
   const SUPABASE_URL = "https://yoziukydjjmnvbvjdgvc.supabase.co";
-  const SUPABASE_KEY = "sb_secret_-gdC6ZEhAaFNVgqjR0gBIw_gDN1gEJA";
+  const SUPABASE_KEY = "sb_publishable_0R_f4QUNYXxwoSiWVJes6g_M7VdO-X3";
 
   window.supabaseClient = createClient(
     SUPABASE_URL,
